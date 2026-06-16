1bfc8e39e5564ec2a8cf58d0674e9e16cd9c1530
GalahadL06 <galahadl06@outlook.com>
2026-06-16 20:22:24 +0800

    Change SELinux to permissive.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -108,9 +108,11 @@ int selinux_enforcing;
 
 static int __init enforcing_setup(char *str)
 {
+	/*
 	unsigned long enforcing;
 	if (!strict_strtoul(str, 0, &enforcing))
 		selinux_enforcing = enforcing ? 1 : 0;
+	*/
 	return 1;
 }
 __setup("enforcing=", enforcing_setup);
